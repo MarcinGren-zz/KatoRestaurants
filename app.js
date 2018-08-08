5ef643f99f79803a9e789bc9e2aa795f33a16f71
Marcin Gren <marcin.gren@gmail.com>
2018-08-08 23:28:47 +0200

    restaurants displays names + imgs now, created link to restaurants

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ app.get('/', function(req, res) {
 })
 
 app.get('/restaurants', function(req, res) {
-    var campgrounds = [
+    var restaurants = [
         { name: 'Hurry Curry', image: 'https://media-cdn.tripadvisor.com/media/photo-s/04/c9/e3/e5/hurry-curry.jpg' },
         { name: 'Bujna', image:'https://scontent.cdninstagram.com/vp/a1b927db49a66d389ed5631e1a8cca78/5BBBCE7A/t51.2885-15/s640x640/sh0.08/e35/32321803_1054602141353815_5992938524339339264_n.jpg' },
         { name: 'Mihiderka', image:'https://retailnet.pl/wp-content/uploads/2017/08/Mihiderka-w-Galerii-Katowickiej.jpg' }
     ]
+
+    res.render('restaurants.ejs', {
+        restaurants: restaurants
+    })
 })
 
 
