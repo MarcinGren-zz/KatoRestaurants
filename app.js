c39e15998b86645fab5b97006aa7ee8a8f9979cb
Marcin Gren <marcin.gren@gmail.com>
2018-08-12 09:41:40 +0200

    Post now creates an object in db

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ var mongoose = require('mongoose')
 
 mongoose.connect(`mongodb://${dbConnection.DB_USERNAME}:${dbConnection.DB_PASSWORD}@ds219532.mlab.com:19532/katorestaurants`)
 
+var restaurantSchema = new mongoose.Schema({
+    name: String,
+    image: String
+})
+var Restaurant = mongoose.model('Restaurant', restaurantSchema)
+
 var restaurants = [
     { name: 'Hurry Curry', image: 'https://media-cdn.tripadvisor.com/media/photo-s/04/c9/e3/e5/hurry-curry.jpg' },
     { name: 'Bujna', image:'https://scontent.cdninstagram.com/vp/a1b927db49a66d389ed5631e1a8cca78/5BBBCE7A/t51.2885-15/s640x640/sh0.08/e35/32321803_1054602141353815_5992938524339339264_n.jpg' },
@@ -30,13 +36,24 @@ app.post('/restaurants', function(req, res) {
     var name = req.body.name
     var image = req.body.image
     var newRestaurant = {name: name, image: image}
-    restaurants.push(newRestaurant)
+    Restaurant.create(newRestaurant, function(err) {
+        console.log(err)
+    })
     res.redirect('/restaurants')
 })
 
 app.get('/restaurants/new', function(req, res) {
     res.render('new.ejs')
 })
+
+// for testing purposes
+// Cat.find({}, function(err, cats) {
+//     console.log(cats)
+// })
+// Cat.create({
+    // name: 'snow'
+    // age: 15,
+// })
 
 
 app.listen(8080, function() {
